468c7466050442010c3111b0e858995bf920751d
Martijn Janssen <martijn9612+github@gmail.com>
2020-03-05 11:23:52 +0100

    add matching per event in the logs

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,9 @@ fn try_main() -> Result<(), Box<dyn std::error::Error>> {
     let mut no_match_counter = 0;
 
     // Regex matching entire line, 2 matching groups, omitting date+time, separated on semicolon
-    let re = Regex::new(r"\d{4}-(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Oct|Sep|Nov|Dec)-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{9}\s(\w+):(.+)").unwrap();
+    // let re = Regex::new(r"\d{4}-(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Oct|Sep|Nov|Dec)-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{9}\s(\w+):(.+)").unwrap();
+    // Shorter regex separating on spaces in the log line, matching on the text around the last ":"
+    let re = Regex::new(r".{11}\s.{18}\s(\w+):(.+)").unwrap();
 
     while let Some(line) = contents.next() {
         let l = line.expect("end of file");
@@ -42,12 +44,43 @@ fn try_main() -> Result<(), Box<dyn std::error::Error>> {
             Some(mtch) => {
                 counter = counter + 1;
 
+                // Match on all log categories
+                match mtch.get(1).unwrap().as_str() {
+                    "NetworkOPs" => {}
+                    "LedgerConsensus" => {}
+                    "LedgerMaster" => {}
+                    "Protocol" => {}
+                    "Peer" => {}
+                    "Application" => {}
+                    "LoadManager" => {}
+                    "LoadMonitor" => {}
+                    "PeerFinder" => {}
+                    "ManifestCache" => {}
+                    "Server" => {}
+                    "Validations" => {}
+                    "Resource" => {}
+                    "Ledger" => {}
+                    "JobQueue" => {}
+                    "NodeStore" => {}
+                    "TaggedCache" => {}
+                    "Amendments" => {}
+                    "OrderBookDB" => {}
+                    "ValidatorList" => {}
+                    "ValidatorSite" => {}
+                    "Flow" => {}
+                    "TimeKeeper" => {}
+                    "InboundLedger" => {}
+                    "TransactionAcquire" => {}
+                    "LedgerHistory" => {}
+                    unknown => println!("encountered unknown event \"{}\"", unknown),
+                }
+
                 // 0 index is the entire match, 1 and 2 are the first 2 capturing groups
-                format!(
-                    "{}: {}",
-                    mtch.get(1).unwrap().as_str(),
-                    mtch.get(2).unwrap().as_str()
-                );
+                // format!(
+                //     "{}: {}",
+                //     mtch.get(1).unwrap().as_str(),
+                //     mtch.get(2).unwrap().as_str()
+                // );
             }
             // None => eprintln!("found no match in line: {}", l),
             None => no_match_counter = no_match_counter + 1,
